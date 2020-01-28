30efb95dac6fc8aced54de190a59499efc8d4554
Andriy Humenetskyy <ahumenetskyy@lohika.com>
2020-01-28 17:12:00 +0200

    users service to docker

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,5 +1,5 @@
 # services/users/project/__init__.py
-
+import os
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
 
@@ -8,7 +8,8 @@ app = Flask(__name__)
 api = Api(app)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 class UserPing(Resource):
     def get(self):
