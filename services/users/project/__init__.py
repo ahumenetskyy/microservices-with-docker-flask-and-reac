4c87739e677bd1ca9c7e81501f0426a386afdb60
Andriy Humenetskyy <ahumenetskyy@lohika.com>
2020-01-29 12:00:32 +0200

    DB container and tests

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -2,6 +2,7 @@
 import os
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 # instance of app
 app = Flask(__name__)
@@ -11,11 +12,27 @@ api = Api(app)
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
 
+# db instance
+db = SQLAlchemy(app)
+
+# models
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(128), nullable=False, default=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+# handle routes
 class UserPing(Resource):
     def get(self):
         return {
             'status': 'success',
-            'message': 'Pong!'
+            'message': 'pong!'
         }
 
-api.add_resource(UserPing, '/user/ping/')
+api.add_resource(UserPing, '/users/ping/')
